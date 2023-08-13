5bac1a5d0f7ee6f2ee036bb50f4f6e66cebbc832
Jin Woo Han <jin00woo@naver.com>
2023-08-13 21:19:56 +0900

    Initial commit.

--- react-django-notes-app/notesapp/notesapp/settings.py
+++ react-django-notes-app/notesapp/notesapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w-74vss9ve+0o_m5)-3(3^uw2g5cvwwrs68-giz7i9@i3ujmc-"
+SECRET_KEY = "django-insecure-$px9oj#7(^9_1&*q=@oczb)cgp1!4_zbguzxmi(v-7+7702pvp"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "api.apps.ApiConfig",
 ]
 
 MIDDLEWARE = [
